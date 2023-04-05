89b6d2e5be6d9b3e3797e0c0f9d60aa44b78e97b
ShockWaveGamer <daniel.fiuk21@gmail.com>
2023-04-04 21:10:06 -0400

    Added Input

--- Assets/Scripts/Graphics/DebugGraphics.cs
+++ Assets/Scripts/Graphics/DebugGraphics.cs
@@ -1,8 +1,6 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Rendering.Universal;
 
 public class DebugGraphics : MonoBehaviour
 {
@@ -11,9 +9,11 @@ public class DebugGraphics : MonoBehaviour
     PlayerInput playerInput;
     #region input actions
 
-    InputAction debugStart, textures;
+    InputAction debugStart, textures, filterMode;
 
     #endregion
+
+    [SerializeField] UniversalRendererData data;
 
     #endregion
 
@@ -25,27 +25,42 @@ public class DebugGraphics : MonoBehaviour
 
     #region variables
 
-    bool debug = false;
+    bool debug = false, colourBlind = false;
 
     #endregion
 
     private void OnEnable()
     {
         playerInput = FindObjectOfType<PlayerInput>();
-        
+
         debugStart = playerInput.actions["Debug Start"];
         debugStart.started += ctx => debug = true;
         debugStart.canceled += ctx => debug = false;
 
         textures = playerInput.actions["Textures"];
         textures.started += ctx => ToggleTextures();
+
+        filterMode = playerInput.actions["Filter Mode"];
+        filterMode.started += ctx => ToggleFilterMode();
     }
 
     private void ToggleTextures()
     {
         if (!debug || !this) return;
         if (!TryGetComponent<GUITextures>(out GUITextures textures)) { Debug.LogError("GRAPHICS GUITexture Component Not Attached To Camera!"); return; }
-        textures.enabled = !textures.enabled; 
+        textures.enabled = !textures.enabled;
         Debug.Log("GRAPHICS: Textures " + (textures.enabled ? "enabled!" : "disabled!"));
+    }
+
+    private void ToggleFilterMode()
+    {
+        if (!debug || !this) return;
+
+        colourBlind = !colourBlind;
+
+        data.rendererFeatures[3].SetActive(!colourBlind);
+        data.rendererFeatures[4].SetActive(colourBlind);
+
+        Debug.Log("GRAPHICS: Filter " + (colourBlind ? "normal!" : "colour blind!"));
     }
 }
